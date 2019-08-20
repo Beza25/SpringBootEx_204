3db79fc60e147925ff0319de75e6d2263f44472c
GBTC405016UR <GBTC405016UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-08-20 15:51:23 -0400

    SpringBootEx_204

---
+++ src/main/java/com/example/demo/Car.java
@@ -0,0 +1,44 @@
+package com.example.demo;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Car {
+
+
+    @NotNull
+    @Size(min=3, max=20)
+    private String name;
+
+    @NotNull
+    @Size(min=2, max=20)
+    private String type;
+
+    @NotNull
+    @Size(min=2, max=15)
+    private String description;
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
